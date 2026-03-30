c02118789720f9fbf9a21036f72baac1832b639f
Arhamys Root <arhamys14@gmail.com>
2026-03-30 21:31:33 +0100

    Crear calse guerrero con herencia

--- src/net/salesianos/Personaje/Guerrero/Guerrero.java
+++ src/net/salesianos/Personaje/Guerrero/Guerrero.java
@@ -1,5 +1,34 @@
 package net.salesianos.Personaje.Guerrero;
 
-public class Guerrero {
-    
+import net.salesianos.Personaje.Personaje;
+
+public class Guerrero extends Personaje {
+    private int defensa;
+
+    public Guerrero(String nombre, int vida, int ataque, int defensa) {
+        super(nombre, vida, ataque);
+        this.defensa = defensa;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public void defender() {
+        System.out.println(nombre + " se defiende con " + defensa + " puntos.");
+    }
+
+    @Override
+    public String toString() {
+        return "Guerrero{" +
+                "nombre='" + nombre + '\'' +
+                ", vida=" + vida +
+                ", ataque=" + ataque +
+                ", defensa=" + defensa +
+                '}';
+    }
 }
