92c846c40e777963b8aaa4cba851aa48b3764dca
Arhamys Root <arhamys14@gmail.com>
2026-03-30 21:26:00 +0100

    Anadir atributos y constructor

--- src/net/salesianos/Personaje/Personaje.java
+++ src/net/salesianos/Personaje/Personaje.java
@@ -1,5 +1,15 @@
 package net.salesianos.Personaje;
 
 public class Personaje {
-    
+     protected String nombre;
+    protected int vida;
+    protected int ataque;
+    public Personaje() {
+    }
+
+    public Personaje(String nombre, int vida, int ataque) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.ataque = ataque;
+    }
 }
