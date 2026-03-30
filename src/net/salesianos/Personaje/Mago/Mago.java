052e1f2b3a1f910b889a6c374594455e51aa4b4d
Arhamys Root <arhamys14@gmail.com>
2026-03-30 21:33:37 +0100

    Crear clase mago y herencia

--- src/net/salesianos/Personaje/Mago/Mago.java
+++ src/net/salesianos/Personaje/Mago/Mago.java
@@ -1,5 +1,34 @@
 package net.salesianos.Personaje.Mago;
 
-public class Mago {
-    
+import net.salesianos.Personaje.Personaje;
+
+public class Mago extends Personaje {
+    private int mana;
+
+    public Mago(String nombre, int vida, int ataque, int mana) {
+        super(nombre, vida, ataque);
+        this.mana = mana;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public void lanzarHechizo() {
+        System.out.println(nombre + " lanza un hechizo usando " + mana + " puntos de maná.");
+    }
+
+    @Override
+    public String toString() {
+        return "Mago{" +
+                "nombre='" + nombre + '\'' +
+                ", vida=" + vida +
+                ", ataque=" + ataque +
+                ", mana=" + mana +
+                '}';
+    }
 }
