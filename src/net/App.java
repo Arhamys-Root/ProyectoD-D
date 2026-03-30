25bc816cbf8fda970cc6ea369b146ba4ab6d93f7
Arhamys Root <arhamys14@gmail.com>
2026-03-30 21:36:10 +0100

    Crear clase APP

--- src/net/App.java
+++ src/net/App.java
@@ -1,5 +1,22 @@
 package net;
 
+import net.salesianos.Personaje.Guerrero.Guerrero;
+import net.salesianos.Personaje.Mago.Mago;
+
 public class App {
-    
+    public static void main(String[] args) {
+
+        Guerrero guerrero = new Guerrero("Arthas", 120, 25, 15);
+        Mago mago = new Mago("Merlín", 80, 30, 100);
+
+        System.out.println(guerrero);
+        guerrero.atacar();
+        guerrero.defender();
+
+        System.out.println();
+
+        System.out.println(mago);
+        mago.atacar();
+        mago.lanzarHechizo();
+    }
 }
